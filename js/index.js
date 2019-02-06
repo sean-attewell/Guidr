0e55bc7b295652de59b44aabf820b76e46b8821f
sean-attewell <sean.attewell@gmail.com>
2019-02-06 11:25:41 +0000

    Menu hover and close button. Also mobile close button position

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
-console.log('Sannity check')
+// console.log('Sannity check')
+
+// toggleMenu is a variable storing what the arrow function returns
 
 const toggleMenu = () => {
     menu.classList.toggle('menu-appear');
@@ -6,11 +8,16 @@ const toggleMenu = () => {
     footer.classList.toggle('footer-blur');
 }
 
+
+// select DOM elements
 const menuButton = document.querySelector('.burger');
 const menu = document.querySelector('.menu');
+const closeMenu = document.querySelector('.closeMenu');
 const container = document.querySelector('.container');
 const footer = document.querySelector('.footer');
 
+
 menuButton.addEventListener('click', toggleMenu);
+closeMenu.addEventListener('click', toggleMenu);
 // toggleMenu is a variable storing what the arrow function returns
 // so don't try and invoke it because it's not a function
